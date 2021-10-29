af04b2e2e6e894c5aba107d86a4d5f994b01d7c6
TruongDiem2k <diemtruong2000@gmail.com>
2021-10-29 18:01:48 +0700

    homework 2

--- index.js
+++ index.js
@@ -5,14 +5,32 @@ fetch(api)
     .then((data) => {
         const people = data.results;
         const htmls = people.map(
-            (item,index) => `
-            <li>
-                <h3>${index+1}: ${item.name}</h3>
+            (item, index) => `
+            <li class="people-item">
+                <h3>${item.name}</h3>
+                <p class="description-${index}"></p>
             </li>
         `
         );
         let html = htmls.join('');
         document.querySelector('.list-people').innerHTML = html;
+
+        const peopleItem = document.querySelectorAll('.people-item');
+        peopleItem.forEach((item, index) => {
+            item.onclick = (event) => {
+                const info = people.find(
+                    (person) => person.name === item.innerText
+                );
+
+                people.forEach((person) => {
+                    if (person.name === item.innerText) {
+                        document.querySelector(
+                            `.description-${index}`
+                        ).innerText = `mass: ${info.mass}, gender: ${info.gender}, skin color: ${info.skin_color}`;
+                    }
+                });
+            };
+        });
     })
     .catch((error) => {
         alert('error');
