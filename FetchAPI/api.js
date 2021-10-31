aa22306592125e419477b70b831e1e2564a7aec6
SuaNgo <okbang20@gmail.com>
2021-10-31 20:56:44 +0700

    fetchAPI

--- FetchAPI/api.js
+++ FetchAPI/api.js
@@ -6,9 +6,11 @@ fetch(endpoint)
      .then((response) => response.json())
      .then((data) => {
         console.log(data.results);
-        for (var i = 0; i < 10; i++){
-            test.innerHTML += data.results[i].name + "<br>";
-            
-        }
+        for (i = 0; i<10; i++){
         
+        document.write('<div class="dropdown">' + '<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">' + data.results[i].name + '</button>' + '<ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">' + '<li><a class="dropdown-item" href="#">' + data.results[i].height + '</a></li>' + '<li><a class="dropdown-item" href="#">' + data.results[i].hair_color + '</a></li>' + '<li><a class="dropdown-item" href="#">' + data.results[i].mass + '</a></li>' + '</ul>' + '</div>')
+        }
 });
+
+
+
